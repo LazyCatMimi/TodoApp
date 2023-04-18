f7190c1c52a54975e44c734926ae40594414683c
Quynh Vo <quynh.vo3314@gmail.com>
2023-04-18 18:56:45 -0400

    set default to login

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName={"Home"}
+        initialRouteName={"Login"}
         screenOptions={{
           headerShown: false,
         }}
